8dbac5b5096966760e06526f3a35722878ed2574
Francisco <77542565+2inthemorningg@users.noreply.github.com>
2021-11-19 20:25:36 +0000

    Update Aplicacao.java

--- src/Aplicacao.java
+++ src/Aplicacao.java
@@ -7,14 +7,14 @@ public class Aplicacao {
     static ArrayList<Cliente> clientes = new ArrayList<>();
 
     public static void main(String[] args){
-        adicionaClientes(new Data(2002,3, 10), new Data(1992, 5, 16), new Data(1989, 10, 3));
+        adicionaClientes();
         login();
     }
 
-    public static void adicionaClientes(Data d, Data d1, Data d2){
-        clientes.add(new Cliente("Francisco", "Coimbra", "fmacedo@student.uc.pt", 967546948, d));
-        clientes.add(new Cliente("Manuel", "Lisboa", "manuel.mail@gmail.com", 915473877, d1));
-        clientes.add(new Cliente("Joana", "Porto", "joana.mail@hotmail.com", 924106841, d2));
+    public static void adicionaClientes(){
+        clientes.add(new Cliente("Francisco", "Coimbra", "fmacedo@student.uc.pt", 969715348, new Data(2002,3, 10)));
+        clientes.add(new Cliente("Manuel", "Lisboa", "manuel.mail@gmail.com", 915473877, new Data(1992, 5, 16)));
+        clientes.add(new Cliente("Joana", "Porto", "joana.mail@hotmail.com", 924106841, new Data(198
     }
 
     public static void login(){
