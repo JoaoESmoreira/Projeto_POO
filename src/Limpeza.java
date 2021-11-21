694379e3d6f4bfac6a4d15f75220554170c8a85e
JoaoESmoreira <jesmoreira2010@gmail.com>
2021-11-21 16:39:53 +0000

    some construction methods were added

--- src/Limpeza.java
+++ src/Limpeza.java
@@ -11,6 +11,35 @@ public class Limpeza extends Produto{
         this.tox = tox;
     }
 
+
+    public int getId() {
+        return super.getId();
+    }
+    public void setId(int id) {
+        super.setId(id);
+    }
+
+    public int getStock() {
+        return super.getStock();
+    }
+    public void setStock(int stock) {
+        super.setStock(stock);
+    }
+
+    public String getNome() {
+        return super.getNome();
+    }
+    public void setNome(String nome) {
+        super.setNome(nome);
+    }
+
+    public void setPreco(int preco) {
+        super.setPreco(preco);
+    }
+    public int getPreco() {
+        return super.getPreco();
+    }
+
     @Override
     public String toString() {
         return "Limpeza{" +
--- src/Mobiliario.java
+++ src/Mobiliario.java
@@ -19,6 +19,35 @@ public class Mobiliario extends Produto{
         this.dim = dim;
     }
 
+
+    public int getId() {
+        return super.getId();
+    }
+    public void setId(int id) {
+        super.setId(id);
+    }
+
+    public int getStock() {
+        return super.getStock();
+    }
+    public void setStock(int stock) {
+        super.setStock(stock);
+    }
+
+    public String getNome() {
+        return super.getNome();
+    }
+    public void setNome(String nome) {
+        super.setNome(nome);
+    }
+
+    public void setPreco(int preco) {
+        super.setPreco(preco);
+    }
+    public int getPreco() {
+        return super.getPreco();
+    }
+
     @Override
     public String toString() {
         return "Mobiliario{" +
